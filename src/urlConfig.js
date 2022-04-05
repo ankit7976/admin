d5beafd7a6d201792f3ca49b8483ac4a9129a48e
ankitvaishnav2021 <87755657+ankitvaishnav2021@users.noreply.github.com>
2022-04-05 12:46:56 +0530

    .

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,6 @@
 
 const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:5400' : 'https://learning-backend-server.herokuapp.com'
 
-
 export const api = `${baseUrl}/api/`;
 export const genratefileName = (filename) =>{
     return `${baseUrl}/public/${filename}`
